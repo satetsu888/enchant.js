f59aa03db1a60c204e247d35c406358e5dd481ac
shotaro.akiyama <satetsu888@gmail.com>
2012-11-18 22:35:07 +0900

    Add Skelton of using People API and making Friends Object wip

--- dev/plugins/mixi.enchant.js
+++ dev/plugins/mixi.enchant.js
@@ -20,6 +20,7 @@ enchant.mixi.init = function(app_id){
         appId:    "mixiapp-web_"+app_id
     });
 
+
     console.log("mixi.auth called");
     mixi.auth({
         scope: "mixi_apps2",
@@ -93,8 +94,65 @@ enchant.mixi.disableScroll = function(){
     mixi.window.scrollTo();
 };
 
-enchant.mixi.PauseButton = enchant.Class.create(enchant.Sprite, {
+enchant.mixi.MixiGame = enchant.Class.create(enchant.Game, {
+    initialize: function(width, height) {
+        enchant.Game.call(this, width, height);
+        this.addEventListener('load', function() {
+            this.currentTime = Date.now();
+            this._intervalID = window.setInterval(function() {
+                game._tick();
+            }, 1000 / this.fps);
+            this.running = true;
+        });
+    },
 
+    loadImage: function(name, src, callback) {
+        if (callback == null) {
+            callback = function() {
+            };
+        }
+        this.assets[name] = enchant.Surface.load(src);
+        this.assets[name].addEventListener('load', callback);
+    },
+
+
+    start: function(){
+        var friends = new enchant.mixi.Friends();
+        var loaded = 0;
+        var loadListener = function(){
+            var e = new enchant.Event('progress');
+            e.loaded = ++loaded;
+            e.total = total;
+            game.dispatchEvent(e);
+            console.log(loaded);
+            console.log(total);
+            if (loaded === total) {
+                game.removeScene(game.loadingScene);
+                game.dispatchEvent(new enchant.Event('load'));
+            }
+        };
+        var o = {};
+        var assets = this._assets.filter(function(asset) {
+            return asset in o ? false : o[asset] = true;
+        });
+        var total = assets.length + friends.datas.length;
+
+        for (i = 0, l = assets.length; i < l; i++) {
+            this.load(assets[i], loadListener);
+        }
+        for(var i=0; i<friends.datas.length; i++){
+            this.loadImage(friends.datas[i].nickname, friends.datas[i].thumbnailUrl,loadListener);
+        }
+        this.pushScene(this.loadingScene);
+    },
+
+    end: function(){
+
+    },
+
+});
+
+enchant.mixi.PauseButton = enchant.Class.create(enchant.Sprite, {
 
     initialize: function(){
         var game = enchant.Game.instance;
@@ -128,6 +186,46 @@ enchant.mixi.PauseButton = enchant.Class.create(enchant.Sprite, {
     },
 
 });
+
+
+enchant.mixi.Friends = enchant.Class.create({
+
+    initialize: function(){
+        if(enchant.mixi.Friends.instance){
+            return enchant.mixi.Friends.instance;
+        }
+
+        this.datas = this.getFriends("all");
+        enchant.mixi.Friends.instance = this;
+    },
+
+    getFriends: function(mode){
+        var friends_array = new Array();
+
+        //API叩く
+
+        //enchant.mixi = { assets: ['https://si0.twimg.com/profile_images/2008781742/icon_reasonably_small.png'] };
+
+        //Friendオブジェクト作る
+        var friend = new enchant.mixi.Friend(100, "IHR",'https://si0.twimg.com/profile_images/2008781742/icon_reasonably_small.png');
+        friends_array.push(friend);
+
+        return friends_array;
+    }
+});
+
+enchant.mixi.Friend = enchant.Class.create( enchant.Sprite,{
+
+    initialize: function(id, nickname, thumbnailUrl){
+        var game = enchant.Game.instance;
+        enchant.Sprite.call(this, 100,100);
+        this.id           = id;
+        this.nickname     = nickname;
+        this.thumbnailUrl = thumbnailUrl;
+    },
+
+});
+
 
 function retrieveGETqs() {
     var qsParm = {};
